85a0d9acfffdd8da23b59cc0c0261775f3d30298
indresh-coder <62210005+indresh-coder@users.noreply.github.com>
2023-04-30 00:33:35 +0530

    assignment 4

---
+++ Assignment4.js
@@ -0,0 +1,30 @@
+// import _ from 'lodash';
+let obj1= {
+    name:"person 1",
+    age: 5
+}
+let obj2={
+    age: 5,
+    name: "person 1"
+}
+let flag=false
+console.log(Object.keys(obj1).length)
+if(Object.keys(obj1).length==Object.keys(obj1).length)
+{
+    for(key in obj1)
+    {
+        if(obj1.key==obj2.key)
+        {
+            flag=true;
+        }
+        else{
+            break;
+        }
+    }
+}
+if(flag){
+    console.log("Objects are equal")
+}
+else{
+    console.log("Objects are not equal")
+}
